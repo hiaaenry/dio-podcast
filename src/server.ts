02f836124a43edc930f10c239212c157ed02c986
Rayaninha <salesrayane11@gmail.com>
2024-08-16 10:54:38 -0300

    feat: simple server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log("Hello World");
+import * as http from "http";
+
+const server = http.createServer(
+  (request: http.IncomingMessage, response: http.ServerResponse) => {}
+);
+
+server.listen(process.env.PORT, () => {
+  console.log("HTTP server running");
+});
