9086a55b19739ad7c22abb5180f8c238beab9aaf
Rayaninha <salesrayane11@gmail.com>
2024-08-16 10:56:04 -0300

    feat: simple controller

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import * as http from "http";
+import { listEpisodes } from "./controllers/podcasts-controller";
 
 const server = http.createServer(
-  (request: http.IncomingMessage, response: http.ServerResponse) => {}
+  async (request: http.IncomingMessage, response: http.ServerResponse) => {
+    if (request.method === "GET") {
+      await listEpisodes(request, response);
+    }
+  }
 );
 
 server.listen(process.env.PORT, () => {
